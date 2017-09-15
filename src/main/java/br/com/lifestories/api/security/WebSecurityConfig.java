1182a1b78f433fbb2b914a10e50f3f6c8a8883a1
--replace-all <joaopedro.dedepaula@gmail.com>
2017-09-15 11:25:02 -0300

    Spring security

--- src/main/java/br/com/lifestories/api/security/WebSecurityConfig.java
+++ src/main/java/br/com/lifestories/api/security/WebSecurityConfig.java
@@ -2,13 +2,10 @@ package br.com.lifestories.api.security;
 
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.session.SessionManagementFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +16,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/**").permitAll();
+        httpSecurity
+                .csrf().disable()
+                .authorizeRequests().antMatchers("/**").permitAll();
                 //.antMatchers("/**").permitAll();
                 //                .antMatchers("/**").authenticated()
                 /*.antMatchers(HttpMethod.POST, "/login").permitAll()
